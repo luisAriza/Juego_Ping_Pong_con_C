e924c55527683ecdc63b6c489c8bd17fe67bb9ec
Luis Ariza <luismanuelariza@hotmail.com>
2021-11-05 20:22:25 -0500

    Raquetas y pelota

--- ping_pong.c
+++ ping_pong.c
@@ -3,6 +3,28 @@
 #define H 75
 
 int main () {
+
+    int pelX, pelY, inijug, finjug, iniia, finia;
+    char campo[V][H];
+
+    pelX = 37;  //La mitad de H... para la pelota
+    pelY = 10;  //La mitad de V
+
+    inijug = 8;  //Inicio de la raqueta del jugador
+    finjug = 12;  //Fin de la raqueta del jugador
+
+    iniia = 8;  //Inicio de la raqueta de la IA
+    finia = 12;  //fin de la raqueta de la IA
+
+    inicio (campo, pelX, pelY, inijug, finjug, iniia, finia);
     system("pause");
     return 0;
+}
+
+void inicio (char campo [V][H], int pelX, int pelY, int inijug, int finjug, int iniia, int finia) {
+    borde (campo);
+}
+
+void borde (char campo [V][H]) {
+    
 }
